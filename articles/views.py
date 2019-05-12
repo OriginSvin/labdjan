9caf896a9b5fa70f0d95ac0416369775efb6bb61
OriginSvin <36486850+OriginSvin@users.noreply.github.com>
2019-05-12 10:07:03 +0300

    Add files via upload

--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,8 @@
+# -*- coding: cp1251 -*-
 from models import Article
 from django.shortcuts import render
 from django.http import Http404
+from django.shortcuts import redirect
 
 def archive(request):
 	return render(request, 'archive.html', {"posts":Article.objects.all()})
@@ -9,4 +11,40 @@ def get_article(request, article_id):
                 post = Article.objects.get(id=article_id)
                 return render(request, 'article.html', {"post": post})
         except Article.DoesNotExist:
+                raise Http404
+def create_post(request):
+        
+        if not request.user.is_anonymous():
+                
+                
+                if request.method == "POST":
+                        form = {
+                                'text': request.POST["text"],
+                                'title': request.POST["title"]
+                                }
+                        
+                        if form["text"] and form["title"]:
+                                chek=True
+                                posts = Article.objects.all()
+                                for post in posts:
+                                        if post.title==form["title"]:
+                                                chek=False
+                                                break
+                                if chek:
+                                
+                                        article=Article.objects.create(text=form["text"],
+                                                               title=form["title"],
+                                                              author=request.user)
+                                        return redirect('get_article', article_id=article.id)
+                                else:
+                                        form['errors'] = u"Имя занято"
+                                        return render(request, 'create_post.html', {'form': form})
+                                        
+                                        
+                        else:
+                                form['errors'] = u"Не все поля заполнены"
+                                return render(request, 'create_post.html', {'form': form})
+                else:
+                        return render(request, 'create_post.html', {})
+        else:
                 raise Http404
--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'articles.views.archive', name='home'),
+    url(r'^article/new/', 'articles.views.create_post', name='create_post'),
     url(r'^article/(?P<article_id>\d+)$','articles.views.get_article',name='get_article'),
     # url(r'^blog/', include('blog.foo.urls')),
 
