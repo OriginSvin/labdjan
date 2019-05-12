32ef95dfb8d04d16c15a0ab8c66711f990f74f21
OriginSvin <36486850+OriginSvin@users.noreply.github.com>
2019-05-12 10:12:33 +0300

    Add files via upload

---
+++ articles/static/js/highlight-post.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	$('.one-post').hover(function(event){
+		console.log("Навели");
+	},
+	function(event){
+		console.log("Вывели");
+	});
+});
+
+$(document).ready(function(){
+	$('.one-post').hover(function(event){
+		$(event.currentTarget).find('.one-post-shadow').animate({opacity: '0.1'}, 300);
+	},
+	function(event){
+		$(event.currentTarget).find('.one-post-shadow').animate({opacity: '0'}, 300);
+	})
+});
