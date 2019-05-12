aef79ecaf3a4be7b60e784a2dc89b04e4d555a46
OriginSvin <36486850+OriginSvin@users.noreply.github.com>
2019-05-12 09:57:51 +0300

    Add files via upload

--- urls.py
+++ urls.py
@@ -6,12 +6,14 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'admin_learning.views.home', name='home'),
-    # url(r'^admin_learning/', include('admin_learning.foo.urls')),
+    #url(r'^$', 'helloworld.views.home', name='home'),
+    # url(r'^helloworld/', include('helloworld.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'flatpages.views.home', name='home'),
+    url(r'^hello/', 'flatpages.views.home', name='home'),	
 )
